e932ed4ae1f6fcaf45650ddc09588b6388c9b8f9
Felix H <felixhuang101@gmail.com>
2024-01-16 11:55:37 -0500

    first commit

--- testutility.py
+++ testutility.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import yaml
 import pandas as pd
+import dask.dataframe as dd
 import datetime 
 import gc
 import re
@@ -34,19 +35,22 @@ def col_header_val(df,table_config):
     df.columns = df.columns.str.replace('[^\w]','_',regex=True)
     df.columns = list(map(lambda x: x.strip('_'), list(df.columns)))
     df.columns = list(map(lambda x: replacer(x,'_'), list(df.columns)))
+    df.columns =list(map(lambda x: x.lower(), list(df.columns)))
+    mycol = list(df.columns)
+    mycol.sort()
+
     expected_col = list(map(lambda x: x.lower(),  table_config['columns']))
     expected_col.sort()
-    df.columns =list(map(lambda x: x.lower(), list(df.columns)))
-    df = df.reindex(sorted(df.columns), axis=1)
-    if len(df.columns) == len(expected_col) and list(expected_col)  == list(df.columns):
+    
+    if len(mycol) == len(expected_col) and list(expected_col)  == list(mycol):
         print("column name and column length validation passed")
         return 1
     else:
         print("column name and column length validation failed")
-        mismatched_columns_file = list(set(df.columns).difference(expected_col))
+        mismatched_columns_file = list(set(mycol).difference(expected_col))
         print("Following File columns are not in the YAML file",mismatched_columns_file)
-        missing_YAML_file = list(set(expected_col).difference(df.columns))
+        missing_YAML_file = list(set(expected_col).difference(mycol))
         print("Following YAML columns are not in the file uploaded",missing_YAML_file)
-        logging.info(f'df columns: {df.columns}')
+        logging.info(f'df columns: {mycol}')
         logging.info(f'expected columns: {expected_col}')
         return 0
